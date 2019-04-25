1ec9149f8c9f501981d42bbcc72e33d9e71e80f8
Masha <himmerka@gmail.com>
2019-04-25 17:46:22 +0300

    Задание номер 10

--- src/test/java/ItemAttributesTest.java
+++ src/test/java/ItemAttributesTest.java
@@ -1,7 +1,6 @@
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.support.Color;
 import selenium.training.TestBase;
 
 public class ItemAttributesTest extends TestBase {
@@ -12,11 +11,7 @@ public class ItemAttributesTest extends TestBase {
         String mainPageItemName = driver.findElement(By.xpath("//div[@class='content']/div[@id='box-campaigns']//li[@class='product column shadow hover-light']//div[@class='name']")).getAttribute("textContent");
         driver.findElement(By.xpath("//div[@class='content']/div[@id='box-campaigns']//li[@class='product column shadow hover-light']")).click();
         String itemPageName = driver.findElement(By.xpath("//div[@id='box-product']//h1")).getAttribute("textContent");
-        if (mainPageItemName.equals(itemPageName)) {
-            System.out.println("Pass");
-        } else {
-            System.out.println("Fail");
-        }
+        Assert.assertEquals(mainPageItemName, itemPageName);
     }
 
     @Test
@@ -27,37 +22,23 @@ public class ItemAttributesTest extends TestBase {
         driver.findElement(By.xpath("//div[@class='content']/div[@id='box-campaigns']//li[@class='product column shadow hover-light']")).click();
         String itemPageRegularPrice = driver.findElement(By.xpath("//div[@class='price-wrapper']/s")).getAttribute("textContent");
         String itemPageCompaignPrice = driver.findElement(By.xpath("//div[@class='price-wrapper']/strong")).getAttribute("textContent");
-        if (mainRegularPrice.equals(itemPageRegularPrice)) {
-            System.out.println("Pass");
-        } else {
-            System.out.println("Fail");
-        }
-        if (mainCompaignPrice.equals(itemPageCompaignPrice)) {
-            System.out.println("Pass");
-        } else {
-            System.out.println("Fail");
-        }
+        Assert.assertEquals(mainRegularPrice, itemPageRegularPrice);
+        Assert.assertEquals(mainCompaignPrice, itemPageCompaignPrice);
     }
 
     @Test
     public void colorMainPagePriceTest() {
         driver.navigate().to("http://localhost:8080/litecart/en/");
-        String mainRegularPrice = driver.findElement(By.xpath("//div[@class='content']/div[@id='box-campaigns']//li[@class='product column shadow hover-light']//div[@class='price-wrapper']/s")).getCssValue("color");
-        String hexRegular = Color.fromString(mainRegularPrice).asHex();
-        System.out.println(hexRegular);
-        if (hexRegular.equals("#777777")) {
-            System.out.println("Pass");
-        } else {
-            System.out.println("Fail");
-        }
-        String mainCompaignPrice = driver.findElement(By.xpath("//div[@class='content']/div[@id='box-campaigns']//li[@class='product column shadow hover-light']//div[@class='price-wrapper']/strong")).getCssValue("color");
-        String hexCompaign = Color.fromString(mainCompaignPrice).asHex();
-        System.out.println(hexCompaign);
-        if (hexCompaign.equals("#cc0000")) {
-            System.out.println("Pass");
-        } else {
-            System.out.println("Fail");
-        }
+        String mainRegularPrice = driver.findElement(By.xpath("//div[@id='box-campaigns']//div[@class='price-wrapper']/s")).getCssValue("color");
+        System.out.println(mainRegularPrice);
+        String[] mainRegPrice = mainRegularPrice.split(",");
+        Assert.assertEquals(mainRegPrice[0], mainRegPrice[1], mainRegPrice[2]);
+        Assert.assertEquals(driver.findElement(By.xpath("//div[@id='box-campaigns']//div[@class='price-wrapper']/s")).getTagName(), "s");
+        String mainCompaignPrice = driver.findElement(By.xpath("//div[@id='box-campaigns']//div[@class='price-wrapper']/strong")).getCssValue("color");
+        System.out.println(mainCompaignPrice);
+        String[] mainComPrice = mainCompaignPrice.split(",");
+        Assert.assertEquals(mainComPrice[1], mainComPrice[2], " 0");
+        Assert.assertEquals(driver.findElement(By.xpath("//div[@id='box-campaigns']//div[@class='price-wrapper']/strong")).getTagName(), "strong");
     }
 
     @Test
@@ -65,54 +46,39 @@ public class ItemAttributesTest extends TestBase {
         driver.navigate().to("http://localhost:8080/litecart/en/");
         driver.findElement(By.xpath("//div[@class='content']/div[@id='box-campaigns']//li[@class='product column shadow hover-light']")).click();
         String itemPageRegularPrice = driver.findElement(By.xpath("//div[@class='price-wrapper']/s")).getCssValue("color");
-        String hexRegular = Color.fromString(itemPageRegularPrice).asHex();
-        System.out.println(hexRegular);
-        if (hexRegular.equals("#666666")) {
-            System.out.println("Pass");
-        } else {
-            System.out.println("Fail");
-        }
+        System.out.println(itemPageRegularPrice);
+        String[] itemRegPrice = itemPageRegularPrice.split(",");
+        Assert.assertEquals(itemRegPrice[0], itemRegPrice[1], itemRegPrice[2]);
+        Assert.assertEquals(driver.findElement(By.xpath("//div[@class='price-wrapper']/s")).getTagName(), "s");
         String itemPageCompaignPrice = driver.findElement(By.xpath("//div[@class='price-wrapper']/strong")).getCssValue("color");
-        String hexCompaign = Color.fromString(itemPageCompaignPrice).asHex();
-        System.out.println(hexCompaign);
-        if (hexCompaign.equals("#cc0000")) {
-            System.out.println("Pass");
-        } else {
-            System.out.println("Fail");
-        }
-}
+        System.out.println(itemPageCompaignPrice);
+        String[] itemComPrice = itemPageCompaignPrice.split(",");
+        Assert.assertEquals(itemComPrice[1], itemComPrice[2], " 0");
+        Assert.assertEquals(driver.findElement(By.xpath("//div[@class='price-wrapper']/strong")).getTagName(), "strong");
+    }
 
     @Test
     public void mainPageSizePriceTest() {
         driver.navigate().to("http://localhost:8080/litecart/en/");
-        Dimension mainPageRegularPrice = driver.findElement(By.xpath("//div[@class='content']/div[@id='box-campaigns']//li[@class='product column shadow hover-light']//div[@class='price-wrapper']/s")).getSize();
-        Dimension mainPageCompaignPrice = driver.findElement(By.xpath("//div[@class='content']/div[@id='box-campaigns']//li[@class='product column shadow hover-light']//div[@class='price-wrapper']/strong")).getSize();
+        String mainPageRegularPrice = driver.findElement(By.xpath("//div[@id='box-campaigns']//div[@class='price-wrapper']/s")).getCssValue("font-size").replace("px", "");
+        Double mainPageRegularPriceDouble = Double.parseDouble(mainPageRegularPrice);
+        String mainPageCompaignPrice = driver.findElement(By.xpath("//div[@id='box-campaigns']//div[@class='price-wrapper']/strong")).getCssValue("font-size").replace("px", "");
+        Double mainPageCompaignPriceDouble = Double.parseDouble(mainPageCompaignPrice);
         System.out.println(mainPageRegularPrice);
         System.out.println(mainPageCompaignPrice);
-        double sizeRegularPrice = mainPageRegularPrice.getHeight() * mainPageRegularPrice.getWidth();
-        double sizeCompaignPrice = mainPageCompaignPrice.getHeight() * mainPageCompaignPrice.getWidth();
-        if (sizeCompaignPrice > sizeRegularPrice) {
-            System.out.println("Pass");
-        } else {
-            System.out.println("Fail");
-        }
+        assert (mainPageRegularPriceDouble < mainPageCompaignPriceDouble);
     }
 
     @Test
     public void itemPageSizePriceTest() {
         driver.navigate().to("http://localhost:8080/litecart/en/");
         driver.findElement(By.xpath("//div[@class='content']/div[@id='box-campaigns']//li[@class='product column shadow hover-light']")).click();
-        Dimension itemPageRegularPrice = driver.findElement(By.xpath("//div[@class='price-wrapper']/s")).getSize();
-        Dimension itemPageCompaignPrice = driver.findElement(By.xpath("//div[@class='price-wrapper']/strong")).getSize();
+        String itemPageRegularPrice = driver.findElement(By.xpath("//div[@class='price-wrapper']/s")).getCssValue("font-size").replace("px", "");
+        Double itemPageRegularPriceDouble = Double.parseDouble(itemPageRegularPrice);
+        String itemPageCompaignPrice = driver.findElement(By.xpath("//div[@class='price-wrapper']/strong")).getCssValue("font-size").replace("px", "");
+        Double itemPageCompaignPriceDouble = Double.parseDouble(itemPageCompaignPrice);
         System.out.println(itemPageRegularPrice);
         System.out.println(itemPageCompaignPrice);
-        double sizeRegularPrice = itemPageRegularPrice.getHeight() * itemPageRegularPrice.getWidth();
-        double sizeCompaignPrice = itemPageCompaignPrice.getHeight() * itemPageCompaignPrice.getWidth();
-        if (sizeCompaignPrice > sizeRegularPrice) {
-            System.out.println("Pass");
-        } else {
-            System.out.println("Fail");
-        }
+        assert (itemPageRegularPriceDouble < itemPageCompaignPriceDouble);
     }
-
 }
